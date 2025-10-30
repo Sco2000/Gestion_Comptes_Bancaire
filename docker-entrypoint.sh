76b3bfad98c21dd8926da4ac470d836072f07bad
Sco2000 <ousmanemarra70@gmail.com>
2025-10-30 21:18:32 +0000

    fix

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,3 +1,15 @@
+#!/bin/sh
+
+# Attendre que la base de données soit prête
+# echo "Waiting for database to be ready..."
+# while ! pg_isready -h $DB_HOST -p $DB_PORT -U $DB_USERNAME; do
+#   echo "Database is unavailable - sleeping"
+#   sleep 1
+# done
+
+# echo "Database is up - executing migrations"
+# php artisan migrate --force
+
 # 🔑 Gestion des clés Passport
 if [ ! -f "storage/oauth-private.key" ] || [ ! -f "storage/oauth-public.key" ]; then
   echo "🔑 Génération des clés Passport..."
